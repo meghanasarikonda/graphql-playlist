6dfc44460863957e5aea85e2af8e0da746589ba3
meghanasarikonda <meghana94987@gmail.com>
2018-09-25 14:36:20 +0530

    connecting to db

--- server/app.js
+++ server/app.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
-
+const mongoose = require('mongoose');
 const app = express();
+
+require('dotenv').config();
+
+mongoose.connect(`mongodb://${process.env.DB_USER}:${process.env.DB_PSWRD}@ds211613.mlab.com:11613/gql-pro`, { useNewUrlParser: true });
+mongoose.connection.once('open', () => {
+  console.log('conneted to database');
+});
 
 app.use('/graphql', graphqlHTTP({
   schema,
