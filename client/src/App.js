42017b2b4063c604d70a52be375ca28902581708
meghanasarikonda <meghana94987@gmail.com>
2018-09-29 14:20:06 +0530

    Client side

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div id="main">
+        <h1>Hello World, Im meghana! </h1>
+      </div>
+    );
+  }
+}
+
+export default App;
