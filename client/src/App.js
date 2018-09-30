28f02c9571ffeab289545f4c0129737adaa5248a
meghanasarikonda <meghana94987@gmail.com>
2018-09-30 23:36:14 +0530

    graphql introduced client

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
+import BookList from './components/BookList.js';
+import AddBook from './components/AddBook.js';
+
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from 'react-apollo';
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000/graphql"
+});
+
 
 class App extends Component {
   render() {
     return (
-      <div id="main">
-        <h1>Hello World, Im meghana! </h1>
-      </div>
+      <ApolloProvider client={client}>
+        <div id="main">
+          <h1>Hello World, Im meghana! </h1>
+          <BookList/>
+          <AddBook/>
+        </div>
+      </ApolloProvider>
     );
   }
 }
