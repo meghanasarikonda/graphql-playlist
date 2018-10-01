f47e9aceab5117866c15e482d0921a6ef952b9bf
meghanasarikonda <meghana94987@gmail.com>
2018-10-01 23:46:49 +0530

    Book details component

---
+++ client/src/components/BookDetails.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { graphql } from 'react-apollo';
+
+class BookDetails extends Component {
+  render(){
+    return(
+      <div id="book-details">
+        <h1>Display book details</h1>
+      </div>
+    );
+  }
+}
+
+export default BookDetails;
